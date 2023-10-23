b496c6dcb797b7da9043ca18e37f5823b1fb1106
fadelfeb <fadelfebriann@gmail.com>
2023-10-24 01:06:51 +0700

    initial commit

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'Fadel'
+export const decsription = 'Svelte blog for poets'
+export const url = dev ? 'http://localhost:5173' : 'http://fadeldev:xyz'
---
+++ src/routes/+layout.ts
@@ -0,0 +1,7 @@
+export const prerender = true
+
+export async function load({ url }: any) {
+	return {
+		url: url.pathname
+	}
+}
